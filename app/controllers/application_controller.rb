89b0593ce011865df3c715e011fd73cb0e207e04
Amanda Steckler <amandajsteckler@gmail.com>
2016-10-16 17:02:03 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,44 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  DELETED_POSTS = []
+
+  get '/' do
+
+  end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    Post.create(name: params[:name], content: params[:content])
+    erb :index
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.all.find(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.all.find(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    Post.update(params[:id], name: params[:name], content: params[:content])
+    redirect "/posts/#{params[:id]}"
+  end
+
+  delete '/posts/:id/delete' do
+    DELETED_POSTS << Post.all.find(params[:id]).name
+    Post.delete(params[:id])
+    redirect '/posts'
   end
 end
